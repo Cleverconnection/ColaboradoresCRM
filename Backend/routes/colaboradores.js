d1ae4ef99db81f737c41eec31db7cb008b0dbb59
Cleverconnection <contato@cleverconnection.com.br>
2025-11-13 22:09:33 -0300

    Delete Backend directory

--- Backend/routes/colaboradores.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { listarColaboradores } from "../controllers/colaboradoresController.js";
-
-const router = express.Router();
-
-// rota GET /api/colaboradores
-router.get("/", listarColaboradores);
-
-export default router;
